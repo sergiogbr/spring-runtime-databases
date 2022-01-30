6ccee496e614be5cbe2a647bbda299b88ba88577
sergiogb <garcia.bravo.sergio@gmail.com>
2022-01-30 13:55:31 +0100

    Adding initial files

---
+++ src/main/kotlin/org/sergiogbr/poc/springruntimedatabases/SpringRuntimeDatabasesApplication.kt
@@ -0,0 +1,11 @@
+package org.sergiogbr.poc.springruntimedatabases
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRuntimeDatabasesApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringRuntimeDatabasesApplication>(*args)
+}
